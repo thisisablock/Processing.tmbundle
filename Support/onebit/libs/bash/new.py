f2cddaf875d8d74a8f623eeef6c04a7b962012d2
Leon Hong <leonshong@gmail.com>
2010-03-14 11:33:52 -0700

    added 'new sketch' command

---
+++ Support/onebit/libs/bash/new.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import commands
+import tempfile
+
+def main(*args):
+    location = args[1]
+    sketch_name = args[2]
+
+    folderpath = "%s/%s" % (location, sketch_name)
+    pdepath = "%s/%s.pde" % (folderpath, sketch_name)
+
+
+    #check if pde file exists
+    if not os.path.isfile(pdepath):
+        if not os.path.exists(folderpath):
+            os.makedirs(folderpath)
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.write("""void setup(){
+
+}
+
+void draw(){
+
+}""")
+        f.close()
+        os.rename(f.name, pdepath)
+    
+    cmd = 'open -b com.macromates.textmate "%s"' % (folderpath)
+    output = commands.getoutput(cmd)
+    return output
+
+if __name__ == "__main__":
+    sys.stdout.write(main(*sys.argv))
--- Support/onebit/onebit.py
+++ Support/onebit/onebit.py
@@ -1,14 +1,8 @@
 from jinjalib import Template, Environment
 import os
 
-# if 'ONEBIT_DEV' in os.environ:
 from jinjalib.loaders import FileSystemLoader
 env = Environment(loader=FileSystemLoader(os.environ['TM_BUNDLE_SUPPORT']+'/onebit/templates'))
-# else:
-#     from jinjalib.code_loaders import FileSystemCodeLoader as FileSystemLoader
-#     env = Environment(loader=FileSystemLoader(os.environ['TM_BUNDLE_SUPPORT']+'/onebit/templatesc'))
-    
-    
 
 import commands
 cmds = (
